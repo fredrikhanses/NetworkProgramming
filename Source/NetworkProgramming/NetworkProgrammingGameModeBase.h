b06e3fb60d761d579abab27aa4c919ce109a9dfb
fredrikhanses <fredrik.hanses@gmail.com>
2020-11-30 15:05:19 +0100

    Base project upload

---
+++ Source/NetworkProgramming/NetworkProgrammingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NetworkProgrammingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NETWORKPROGRAMMING_API ANetworkProgrammingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
