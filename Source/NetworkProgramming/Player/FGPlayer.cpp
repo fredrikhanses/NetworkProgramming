034b5498ce9aa33c8523c338d55094140e8fd97e
fredrikhanses <fredrik.hanses@gmail.com>
2020-12-01 16:35:11 +0100

    Video processed

---
+++ Source/NetworkProgramming/Player/FGPlayer.cpp
@@ -0,0 +1,142 @@
+#include "FGPlayer.h"
+#include "Components/InputComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/SphereComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Engine/NetDriver.h"
+#include "GameFramework/PlayerState.h"
+#include "../Components/FGMovementComponent.h"
+#include "../FGMovementStatics.h"
+
+AFGPlayer::AFGPlayer()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionComponent"));
+	RootComponent = CollisionComponent;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetupAttachment(CollisionComponent);
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComponent->bInheritYaw = false;
+	SpringArmComponent->SetupAttachment(CollisionComponent);
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+
+	MovementComponent = CreateDefaultSubobject<UFGMovementComponent>(TEXT("MovementComponent"));
+
+	SetReplicateMovement(false);
+}
+
+void AFGPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+	MovementComponent->SetUpdatedComponent(CollisionComponent);
+
+	//CreateDebugWidget();
+	//if (DebugMenuInstance != nullptr)
+	//{
+	//	DebugMenuInstance
+	//}
+}
+
+void AFGPlayer::Tick(float DeltaTime)
+{
+	 Super::Tick(DeltaTime);
+
+	 const float Friction = IsBraking() ? BrakingFriction : DefaultFriction;
+	 const float Alpha = FMath::Clamp(FMath::Abs(MovementVelocity / (MaxVelocity * 0.75f)), 0.0f, 1.0f);
+	 const float TurnSpeed = FMath::InterpEaseInOut(0.0f, TurnSpeedDefault, Alpha, 5.0f);
+	 const float MovementDirection = MovementVelocity > 0.0f ? Turn : -Turn;
+
+	 Yaw += (MovementDirection * TurnSpeed) * DeltaTime;
+	 FQuat WantedFacingDirection = FQuat(FVector::UpVector, FMath::DegreesToRadians(Yaw));
+	 MovementComponent->SetFacingRotation(WantedFacingDirection);
+
+	 FFGFrameMovement FrameMovement = MovementComponent->CreateFrameMovement();
+
+	 MovementVelocity += Forward * Acceleration * DeltaTime;
+	 MovementVelocity = FMath::Clamp(MovementVelocity, -MaxVelocity, MaxVelocity);
+	 MovementVelocity *= FMath::Pow(Friction, DeltaTime);
+
+	 MovementComponent->ApplyGravity();
+	 FrameMovement.AddDelta(GetActorForwardVector() * MovementVelocity * DeltaTime);
+	 MovementComponent->Move(FrameMovement);
+
+	 if (IsLocallyControlled())
+	 {
+		 Server_SendLocation(GetActorLocation());
+		 Server_SendRotation(GetActorRotation().Quaternion());
+	 }
+}
+
+void AFGPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("Accelerate"), this, &AFGPlayer::Handle_Acceleration);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &AFGPlayer::Handle_Turn);
+
+	PlayerInputComponent->BindAction(TEXT("Brake"), IE_Pressed, this, &AFGPlayer::Handle_BrakePressed);
+	PlayerInputComponent->BindAction(TEXT("Brake"), IE_Released, this, &AFGPlayer::Handle_BrakeReleased);
+}
+
+int32 AFGPlayer::GetPing() const
+{
+	if (GetPlayerState())
+	{
+		return static_cast<int32>(GetPlayerState()->GetPing());
+	}
+
+	return 0;
+}
+
+void AFGPlayer::Server_SendLocation_Implementation(const FVector& LocationToSend)
+{
+	MultiCast_SendLocation(LocationToSend);
+}
+
+void AFGPlayer::MultiCast_SendLocation_Implementation(const FVector& LocationToSend)
+{
+	if (!IsLocallyControlled())
+	{
+		SetActorLocation(LocationToSend);
+	}
+}
+
+void AFGPlayer::Server_SendRotation_Implementation(const FQuat& RotationToSend)
+{
+	MultiCast_SendRotation(RotationToSend);
+}
+
+void AFGPlayer::MultiCast_SendRotation_Implementation(const FQuat& RotationToSend)
+{
+	if (!IsLocallyControlled())
+	{
+		MovementComponent->SetFacingRotation(RotationToSend);
+	}
+}
+
+void AFGPlayer::Handle_Acceleration(float Value)
+{
+	Forward = Value;
+}
+
+void AFGPlayer::Handle_Turn(float Value)
+{
+	Turn = Value;
+}
+
+void AFGPlayer::Handle_BrakePressed()
+{
+	bBrake = true ;
+}
+
+void AFGPlayer::Handle_BrakeReleased()
+{
+	bBrake = false;
+}
---
+++ Source/NetworkProgramming/Player/FGPlayer.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "FGPlayer.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+class UFGMovementComponent;
+class UStaticMeshComponent;
+class USphereComponent;
+
+UCLASS()
+class NETWORKPROGRAMMING_API AFGPlayer : public APawn
+{
+	GENERATED_BODY()
+
+public:
+
+	AFGPlayer();
+
+protected:
+
+	virtual void BeginPlay();
+
+public:
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere, Category = Movement)
+	float Acceleration = 500.0f;
+
+	UPROPERTY(EditAnywhere, Category = Movement, meta = (DisplayName = "TurnSpeed"))
+	float TurnSpeedDefault = 100.0f;
+
+	UPROPERTY(EditAnywhere, Category = Movement)
+	float MaxVelocity = 2000.0f;
+
+	UPROPERTY(EditAnywhere, Category = Movement, meta = (ClampMin = 0.0f, ClampMax = 1.0f))
+	float DefaultFriction = 0.75f;
+
+	UPROPERTY(EditAnywhere, Category = Movement, meta = (ClampMin = 0.0f, ClampMax = 1.0f))
+	float BrakingFriction = 0.001f;
+
+	UFUNCTION(BlueprintPure)
+	bool IsBraking() const { return bBrake; }
+
+	UFUNCTION(BlueprintPure)
+	int32 GetPing() const;
+
+	UFUNCTION(Server, Unreliable)
+	void Server_SendLocation(const FVector& LocationToSend);
+
+	UFUNCTION(NetMulticast, Unreliable)
+	void MultiCast_SendLocation(const FVector& LocationToSend);
+
+	UFUNCTION(Server, Unreliable)
+	void Server_SendRotation(const FQuat& RotationToSend);
+
+	UFUNCTION(NetMulticast, Unreliable)
+	void MultiCast_SendRotation(const FQuat& RotationToSend);
+
+private:
+	
+	void Handle_Acceleration(float Value);
+	void Handle_Turn(float Value);
+	void Handle_BrakePressed();
+	void Handle_BrakeReleased();
+
+	float Forward = 0.0f;
+	float Turn = 0.0f;
+
+	float MovementVelocity = 0.0f;
+	float Yaw = 0.0f;
+
+	bool bBrake = false;
+
+	UPROPERTY(EditAnywhere, Category = Collision)
+	USphereComponent* CollisionComponent;
+	
+	UPROPERTY(EditAnywhere, Category = Mesh)
+	UStaticMeshComponent* MeshComponent;
+	
+	UPROPERTY(EditAnywhere, Category = Camera)
+	USpringArmComponent* SpringArmComponent;
+	
+	UPROPERTY(EditAnywhere, Category = Camera)
+	UCameraComponent* CameraComponent;
+	
+	UPROPERTY(EditAnywhere, Category = Movement)
+	UFGMovementComponent* MovementComponent;
+};
